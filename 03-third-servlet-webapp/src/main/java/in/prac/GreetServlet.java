3ae3d50e3731c80c7fb27e8dcbf054eba9f1e515
sweety6789 <stardom374@gmail.com>
2025-03-23 21:47:51 +0530

    3rd project

---
+++ 03-third-servlet-webapp/src/main/java/in/prac/GreetServlet.java
@@ -0,0 +1,58 @@
+package in.prac;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Timestamp;
+import java.time.LocalTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GreetServlet
+ */
+@WebServlet("/GreetServlet")
+public class GreetServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GreetServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Get current time
+        LocalTime now = LocalTime.now();
+        int hour = now.getHour();
+
+        // Determine greeting based on time
+        String greeting;
+        if (hour >= 6 && hour < 12) {
+            greeting = "Good Morning!";
+        } else if (hour >= 12 && hour < 17) {
+            greeting = "Good Afternoon!";
+        } else if (hour >= 17 && hour < 21) {
+            greeting = "Good Evening!";
+        } else {
+            greeting = "Good Night!";
+        }
+
+        // Display greeting
+        System.out.println(greeting);
+        // TODO Auto-generated method stub
+		PrintWriter writer = response.getWriter();
+		writer.append("<h1>"+greeting + " Tiklu</h1>");
+//		.append(request.getContextPath());
+	}
+
+}
